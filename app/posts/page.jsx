6aa6f44155cff6b041c5ac5746d2432e02b3e7cc
Ebrahimim29 <Ebrahimim29@gmail.com>
2026-07-26 06:37:48 +0330

    Serve-Json in Project

--- app/posts/page.jsx
+++ app/posts/page.jsx
@@ -1,14 +1,21 @@
-import { Button, ListGroup, TableRow } from "flowbite-react";
-import ListGroupItemLink from "./components/ListGroupItemLink";
+import { Button, ListGroup } from "flowbite-react";
+import ListGroupItemLink from "../components/ListGroupItemLink";
 
+const getPostsService = async () => {
+    const res = await fetch('http://localhost:4000/posts')
+    const posts = await res.json()
+    return posts
+}
 
-const Page = () => {
+const Page = async () => {
+    const posts = await getPostsService()
+
     return (
         <div className="flex flex-col justify-center items-center min-h-screen">
             <Button color="blue">Back</Button>
             <ListGroup className="mt-3 w-full">
-                {props.posts.map(p => (
-                    <ListGroupItemLink key={p.id} href={'/posts/${p.id'} title={p.title}></ListGroupItemLink>
+                {posts.map(p => (
+                    <ListGroupItemLink key={p.id} href={`/posts/${p.id}`} title={p.title}/>
                 ))}
             </ListGroup>
         </div>

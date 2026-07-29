8f3600817274d1f7ca4949c2109ddb848fc583a7
Ebrahimim29 <Ebrahimim29@gmail.com>
2026-07-29 07:03:51 +0330

    Data Fetching(SSG)-part2

--- app/posts/[postId]/page.jsx
+++ app/posts/[postId]/page.jsx
@@ -2,6 +2,15 @@ import { ListGroup } from "flowbite-react";
 import BackButton from "../../components/BackButton";
 import ListGroupItemLink from "../../components/ListGroupItemLink";
 
+export async function generateStaticParams () {
+    return [
+        {postId: "1"},
+        {postId: "2"},
+        {postId: "3"}
+    ]
+}
+
+
 const getPostsService = async (postId) => {
     const res = await fetch(`http://localhost:4000/posts/${postId}`)
     const post = await res.json()
@@ -11,12 +20,14 @@ const getPostsService = async (postId) => {
 
 const Page = async ({ params }) => {
 
-    const post = await getPostsService(params.postId)
+    const {postId} = await params;
+
+    const post = await getPostsService(postId)
 
     return (
         <div>
             <BackButton />
-            <ListGroup className="flex flex-col justify-center items-center w-100 h-100 bg-green-400">
+            <ListGroup className="flex flex-col justify-center items-center w-full h-full bg-green-400">
                 <ListGroupItemLink title={post.id}/>
                 <ListGroupItemLink title={post.title}/>
             </ListGroup>
